6acf63342665b721243a0fc9057eb15394cebff7
Thiviru <thiviru1000@gmail.com>
2025-05-05 20:18:32 +1000

    addded unet img

--- App/pages/About.py
+++ App/pages/About.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-# Set up the page configuration
-st.set_page_config(
-    page_title="About",
-    page_icon="👀"
-)
-
 def run_app() -> None:
+    # Set up the page configuration
+    st.set_page_config(
+        page_title="About",
+        page_icon="👀"
+    )
 
     # Title 
     st.title("About us")
@@ -14,7 +13,7 @@ def run_app() -> None:
     # First Row: About the App 
     col1, col2 = st.columns(2)
     with col1:
-        st.image("path_to_medai_image.png", caption="Another picture related to MedAI", use_column_width=True)
+        st.image("", caption="Another picture related to MedAI", use_container_width=True)
     with col2:
         st.subheader("More detailed description of MedAI's usage")
         st.markdown("""
@@ -30,20 +29,15 @@ def run_app() -> None:
     st.header("Dataset used")
 
     # Second Row: Dataset Details 
-    col3, col4 = st.columns(2)
-    with col3:
-        st.subheader("Description of our dataset")
-        st.markdown("""
-            The dataset used is from the Foot Ulcer Segmentation Challenge:
-            - 810 training images + masks  
-            - 200 validation images + masks  
-            - 200 testing images (no masks)  
+    st.subheader("Description of our dataset")
+    st.markdown("""
+        The dataset used is from the Foot Ulcer Segmentation Challenge:
+        - 810 training images + masks  
+        - 200 validation images + masks  
+        - 200 testing images (no masks)  
 
-            We performed data augmentation with Albumentations, increasing the dataset to 4860 training samples. Each image is paired with a binary mask used for segmentation.
-        """)
-
-    with col4:
-        st.image("path_to_sample_image.png", caption="Pictures of samples from our dataset", use_column_width=True)
+        We performed data augmentation with Albumentations, increasing the dataset to 4860 training samples. Each image is paired with a binary mask used for segmentation.
+    """)
 
 
 if __name__ == "__main__":
--- App/pages/Contact.py
+++ App/pages/Contact.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
-# Set up the page configuration
-st.set_page_config(
-    page_title="Contact Us",
-    page_icon="✉️"
-)
+def run_app() -> None:
+    # Set up the page configuration
+    st.set_page_config(
+        page_title="Contact Us",
+        page_icon="✉️"
+    )
+    
+    st.title("Contact us")
+
+    st.markdown("### Thiviru Gunawardena – 14542791 – [thirivu.gunawardena@student.uts.edu.au](mailto:thirivu.gunawardena@student.uts.edu.au)")
+    st.markdown("### Jachym Zamouril – 25639802 – [Jachym.Zamouril@student.uts.edu.au](mailto:Jachym.Zamouril@student.uts.edu.au)")
+    st.markdown("### Carlos Daroy – 24752370 – [Carlos.Daroy@student.uts.edu.au](mailto:Carlos.Daroy@student.uts.edu.au)")
+
+if __name__ == "__main__":
+    run_app()
