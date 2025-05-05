1064782c60168aaecb268a3d3984bfc7f04ccc03
Thiviru <thiviru1000@gmail.com>
2025-05-05 21:02:26 +1000

    changes added

--- App/pages/Contact.py
+++ App/pages/Contact.py
@@ -7,11 +7,27 @@ def run_app() -> None:
         page_icon="✉️"
     )
 
-    st.title("Contact us")
+    st.header("Contact us", divider=True)
 
-    st.markdown("### Thiviru Gunawardena – 14542791 – [thirivu.gunawardena@student.uts.edu.au](mailto:thirivu.gunawardena@student.uts.edu.au)")
+    st.markdown("""
+        If you have questions about the project, would like to report an issue, or are interested in collaboration, feel free to contact us via the emails below.
+    """)
+
+    st.markdown("""
+    ### 🧑‍💻 Team Members & Roles
+    - **Thiviru Gunawardena (14542791)** – Data Collection, Model Training, & Model Deployment
+    - **Jachym Zamouril (25639802)** – Frontend Development & Documentation
+    - **Carlos Daroy (24752370)** – Model Evaluation, Area Calculation, & Documentation  
+    """)
+
+    st.markdown("### Thiviru Gunawardena – 14542791 – [thiviru.gunawardena@student.uts.edu.au](mailto:thiviru.gunawardena@student.uts.edu.au)")
     st.markdown("### Jachym Zamouril – 25639802 – [Jachym.Zamouril@student.uts.edu.au](mailto:Jachym.Zamouril@student.uts.edu.au)")
     st.markdown("### Carlos Daroy – 24752370 – [Carlos.Daroy@student.uts.edu.au](mailto:Carlos.Daroy@student.uts.edu.au)")
 
+    st.markdown("""
+    ### 📁 Additional Links
+    * [Project GitHub Repository](https://github.com/Thivirug/Assessment3)
+    * [Video Pitch](To be added)
+    """)
 if __name__ == "__main__":
     run_app()
