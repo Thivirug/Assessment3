c8ab1058c2e19f6076930be00e73c169c8c13945
Thiviru <thiviru1000@gmail.com>
2025-05-05 20:39:31 +1000

    changes

--- App/pages/About.py
+++ App/pages/About.py
@@ -1,6 +1,30 @@
 import streamlit as st
 import os
 
+def model_info() -> None:
+    """
+        Display the model information.
+    """
+    st.subheader("Model Information", divider=True)
+    st.markdown("""
+        The model used is a U-Net architecture, which is a convolutional neural network (CNN) designed for image segmentation tasks. 
+        It consists of an encoder-decoder structure with skip connections, allowing it to capture both local and global features effectively.
+    """)
+
+def dataset_info() -> None:
+    """
+        Display the dataset information.
+    """
+    st.subheader("Dataset Information", divider=True)
+    st.markdown("""
+        The dataset used is from the Foot Ulcer Segmentation Challenge:
+        * 810 training images + masks  
+        * 200 validation images + masks  
+        * 200 testing images (no masks)  
+
+        We performed data augmentation with Albumentations, increasing the dataset to 4860 training samples. Each image is paired with a binary mask used for segmentation.
+    """)
+
 def run_app() -> None:
     # Set up the page configuration
     st.set_page_config(
@@ -10,8 +34,6 @@ def run_app() -> None:
 
     # Title 
     st.header("About", divider=True)
-
-    # About the App 
     
     # Determine the absolute path to the directory containing the script
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -21,31 +43,15 @@ def run_app() -> None:
     # Construct the absolute path to the destination file
     img_dir = os.path.join(BASE_DIR, "App", "Assets", "unet.jpg")
     st.image(img_dir, caption="The U-Net", use_container_width=True)
-    
-    st.subheader("More detailed description of MedAI's usage", divider=True)
-    st.markdown("""
-        Our application uses deep learning to segment foot ulcers from clinical images. This allows medical professionals to quickly and accurately assess the size and severity of wounds.  
-        
-        The goal is to provide an easy-to-use, real-time tool that automates wound segmentation and helps calculate surface area using a reference marker.
-    """)
+
+    # Model information
+    model_info()
 
     # Divider
     st.markdown("---")
 
-    # Dataset Used Header 
-    st.header("Dataset used")
-
-    # Second Row: Dataset Details 
-    st.subheader("Description of our dataset")
-    st.markdown("""
-        The dataset used is from the Foot Ulcer Segmentation Challenge:
-        - 810 training images + masks  
-        - 200 validation images + masks  
-        - 200 testing images (no masks)  
-
-        We performed data augmentation with Albumentations, increasing the dataset to 4860 training samples. Each image is paired with a binary mask used for segmentation.
-    """)
-
-
+    # Dataset information
+    dataset_info()
+    
 if __name__ == "__main__":
     run_app()
