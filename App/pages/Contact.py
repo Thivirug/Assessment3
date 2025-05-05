bcf71b1d9a927cd1287bbca2ae188a951b7b3d32
Thiviru <thiviru1000@gmail.com>
2025-05-05 21:11:03 +1000

    check change

--- App/pages/Contact.py
+++ App/pages/Contact.py
@@ -16,7 +16,8 @@ def run_app() -> None:
     st.markdown("""
     ### 🧑‍💻 Team Members & Roles
     - **Thiviru Gunawardena (14542791) : [thiviru.gunawardena@student.uts.edu.au](mailto:thiviru.gunawardena@student.uts.edu.au)** 
-                * Data Collection, Model Training, & Model Deployment
+            
+            * Data Collection, Model Training, & Model Deployment
     - **Jachym Zamouril (25639802) : [Jachym.Zamouril@student.uts.edu.au](mailto:Jachym.Zamouril@student.uts.edu.au)** – Frontend Development & Documentation
     - **Carlos Daroy (24752370) : [Carlos.Daroy@student.uts.edu.au](mailto:Carlos.Daroy@student.uts.edu.au)** – Model Evaluation, Area Calculation, & Documentation  
     """)
